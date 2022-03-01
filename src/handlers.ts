9f604d48c6842ae8c763469620537667cbf8e598
Newton Lomar <newtonlomar16@gmail.com>
2022-03-01 20:58:37 -0300

    builded create(POST) and Read(GET) endpoints

--- src/handlers.ts
+++ src/handlers.ts
@@ -3,14 +3,22 @@ import AWS from "aws-sdk";
 import { v4 } from "uuid";
 
 const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = "ProductsTable";
 
-export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  await docClient.put({
-    TableName: "ProductsTable",
-    Item: {
-      productID: v4(),
-    },
-  });
+export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const reqBody = JSON.parse(event.body as string);
+
+  const product = {
+    ...reqBody,
+    productID: v4(),
+  };
+
+  await docClient
+    .put({
+      TableName: tableName,
+      Item: product,
+    })
+    .promise();
 
   return {
     statusCode: 200,
@@ -22,5 +30,30 @@ export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProx
       null,
       2,
     ),
+  };
+};
+
+export const getProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const id = event.pathParameters?.id;
+
+  const output = await docClient
+    .get({
+      TableName: tableName,
+      Key: {
+        productID: id,
+      },
+    })
+    .promise();
+
+  if (output.Item) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "not found" }),
+    };
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(output.Item),
   };
 };
