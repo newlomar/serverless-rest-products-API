c3efa89751a645dc175d3d7eec9d7f6c4d286c90
Newton Lomar <newtonlomar16@gmail.com>
2022-03-01 15:39:09 -0300

    servless iam, events and function configuration + first API steps

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,6 +1,17 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
+import { v4 } from "uuid";
+
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  await docClient.put({
+    TableName: "ProductsTable",
+    Item: {
+      productID: v4(),
+    },
+  });
+
   return {
     statusCode: 200,
     body: JSON.stringify(
